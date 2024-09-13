d3ea1c1c206ee0b5fffda09cfc33bdfc8c8b5dc3
yubenbauty <yubenbauty@gmail.com>
2024-09-14 00:02:51 +0700

    feat: add header and total balance in home and sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/totalBalanceBox";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Yuben" };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <HeaderBox
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || "Guest"}
+          subtext="Access and manage your account and transaction efficiently."
+        />
+        <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={69000.56}
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Home;
---
+++ components/Sidebar.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const Sidebar = ({ user }: SiderbarProps) => {
+  const pathName = usePathname();
+  return (
+    <section className="sidebar">
+      <nav className="flex flex-col gap-4">
+        <Link href="/" className="mb-12 flex cursor-pointer items-center gap-2">
+          <Image
+            src="/icons/logo.svg"
+            width={34}
+            height={34}
+            alt="Calyben logo"
+            className="size-[24px] max-xl:size-14"
+          />
+          <h1 className="sidebar-logo">Calyben</h1>
+        </Link>
+
+        {sidebarLinks.map((items) => {
+          const isActive =
+            pathName === items.route || pathName.startsWith(`${items.route}/`);
+          return (
+            <Link
+              href={items.route}
+              key={items.label}
+              className={cn("flex items-center gap-2 p-2 rounded-md", {
+                "bg-bank-gradient": isActive,
+              })}
+            >
+              <div className="relative flex items-center justify-center w-8 h-8">
+                <Image
+                  src={items.imgURL}
+                  alt={items.label}
+                  fill
+                  className={cn("object-contain", {
+                    "brightness-[3] invert-0": isActive,
+                  })}
+                />
+              </div>
+              <p
+                className={cn("sidebar-label text-sm", {
+                  "!text-white": isActive,
+                })}
+              >
+                {items.label}
+              </p>
+            </Link>
+          );
+        })}
+      </nav>
+    </section>
+  );
+};
+
+export default Sidebar;
