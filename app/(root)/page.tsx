92cf3abbd37b51ae1489332d5775fe57076e9a23
yubenbauty <yubenbauty@gmail.com>
2024-09-14 01:06:30 +0700

    add right sidbar for showing profile detail and bank card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,25 +1,38 @@
 import React from "react";
 import HeaderBox from "@/components/HeaderBox";
 import TotalBalanceBox from "@/components/totalBalanceBox";
+import RightSidebar from "@/components/RIghtSidebar";
 
 const Home = () => {
-  const loggedIn = { firstName: "Yuben" };
+  const loggedIn = {
+    firstName: "Yuben",
+    lastName: "Cika",
+    email: "Yubencika@mail.com",
+  };
 
   return (
     <section className="home">
       <div className="home-content">
-        <HeaderBox
-          type="greeting"
-          title="Welcome"
-          user={loggedIn?.firstName || "Guest"}
-          subtext="Access and manage your account and transaction efficiently."
-        />
-        <TotalBalanceBox
-          accounts={[]}
-          totalBanks={1}
-          totalCurrentBalance={69000.56}
-        />
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transaction efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={69000.56}
+          />
+        </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 634534 }, { currentBalance: 634534 }]}
+      />
     </section>
   );
 };
